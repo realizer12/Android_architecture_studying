3380a059cc4bdeb25e33177791a3edbceef3fbcf
이동훈 <dlwldlwldus123@gmail.com>
2022-11-22 23:34:27 +0900

    HW-5 베이스뷰모델 주석 추가

--- presentation/src/main/java/com/example/presentation/base/BaseViewModel.kt
+++ presentation/src/main/java/com/example/presentation/base/BaseViewModel.kt
@@ -5,6 +5,9 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.kotlin.addTo
 
+/**
+ * 베이스가 되는 뷰모델이다. 
+**/
 open class BaseViewModel:ViewModel() {
     private val compositeDisposable = CompositeDisposable()
 
