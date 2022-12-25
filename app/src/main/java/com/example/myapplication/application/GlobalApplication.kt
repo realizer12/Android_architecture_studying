fe26611a2cc80fe3b420a5307ad536e65067ace9
이동훈 <dlwldlwldus123@gmail.com>
2022-12-25 19:07:55 +0900

    HW-8 ->  global application에  hilt annotaion 적용

--- app/src/main/java/com/example/myapplication/application/GlobalApplication.kt
+++ app/src/main/java/com/example/myapplication/application/GlobalApplication.kt
@@ -3,8 +3,10 @@ package com.example.myapplication.application
 import android.app.Application
 import com.example.myapplication.BuildConfig
 import com.example.myapplication.timber.TimberCustomTree
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class GlobalApplication: Application() {
     override fun onCreate() {
         super.onCreate()
